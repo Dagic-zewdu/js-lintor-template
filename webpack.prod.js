c17225d0b3bd399b260b342e34e7d971a5a3cfe6
Dagi <dagi.zewdu.dz@gmail.com>
2022-02-15 18:03:50 +0300

    Added webpack configuration

--- webpack.prod.js
+++ webpack.prod.js
@@ -8,7 +8,7 @@ module.exports = merge(common, {
   mode: 'production',
   output: {
     filename: '[name].[contentHash].bundle.js',
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, 'dist'),
   },
   plugins: [
     new CleanWebpackPlugin(),
